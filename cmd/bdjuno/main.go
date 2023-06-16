e94f8b25e03d990402f019fd28a973e50e236390
raifpy <omerto12.45@gmail.com>
2023-06-16 02:15:04 +0200

    ahah, gitignore

---
+++ cmd/bdjuno/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"github.com/CosmWasm/wasmd/x/wasm"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/forbole/juno/v5/cmd"
+	initcmd "github.com/forbole/juno/v5/cmd/init"
+	parsetypes "github.com/forbole/juno/v5/cmd/parse/types"
+	startcmd "github.com/forbole/juno/v5/cmd/start"
+	"github.com/forbole/juno/v5/modules/messages"
+
+	migratecmd "github.com/forbole/bdjuno/v5/cmd/migrate"
+	parsecmd "github.com/forbole/bdjuno/v5/cmd/parse"
+
+	"github.com/forbole/bdjuno/v5/types/config"
+
+	"cosmossdk.io/simapp"
+	"github.com/forbole/bdjuno/v5/database"
+	"github.com/forbole/bdjuno/v5/modules"
+)
+
+func main() {
+	initCfg := initcmd.NewConfig().
+		WithConfigCreator(config.Creator)
+
+	parseCfg := parsetypes.NewConfig().
+		WithDBBuilder(database.Builder).
+		WithEncodingConfigBuilder(config.MakeEncodingConfig(getBasicManagers())).
+		WithRegistrar(modules.NewRegistrar(getAddressesParser()))
+
+	cfg := cmd.NewConfig("bdjuno").
+		WithInitConfig(initCfg).
+		WithParseConfig(parseCfg)
+
+	// Run the command
+	rootCmd := cmd.RootCmd(cfg.GetName())
+
+	rootCmd.AddCommand(
+		cmd.VersionCmd(),
+		initcmd.NewInitCmd(cfg.GetInitConfig()),
+		parsecmd.NewParseCmd(cfg.GetParseConfig()),
+		migratecmd.NewMigrateCmd(cfg.GetName(), cfg.GetParseConfig()),
+		startcmd.NewStartCmd(cfg.GetParseConfig()),
+	)
+
+	executor := cmd.PrepareRootCmd(cfg.GetName(), rootCmd)
+	err := executor.Execute()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// getBasicManagers returns the various basic managers that are used to register the encoding to
+// support custom messages.
+// This should be edited by custom implementations if needed.
+func getBasicManagers() []module.BasicManager {
+	return []module.BasicManager{
+		simapp.ModuleBasics,
+		module.NewBasicManager(wasm.AppModuleBasic{}),
+	}
+}
+
+// getAddressesParser returns the messages parser that should be used to get the users involved in
+// a specific message.
+// This should be edited by custom implementations if needed.
+func getAddressesParser() messages.MessageAddressesParser {
+	return messages.JoinMessageParsers(
+		messages.CosmosMessageAddressesParser,
+	)
+}
